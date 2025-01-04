d6adf781029c4a5dea5b0d9441700aaba3bc519a
Sachin kedar <kedarsachin1020@gmail.com>
2025-01-04 11:58:25 +0530

    Done

---
+++ Arrays.js
@@ -0,0 +1,18 @@
+/*
+
+1. In JavaScript, arrays aren't primitives but are instead Array objects.
+
+2. JavaScript arrays are resizable and can contain a mix of different data types. 
+
+3. JavaScript array-copy operations create shallow copies.
+    ex. 
+        const fruits = ["Strawberry", "Mango"];
+
+        // Create a copy using spread syntax.
+        const fruitsCopy = [...fruits];
+
+        console.log(fruitsCopy);
+
+
+*/
+ 
