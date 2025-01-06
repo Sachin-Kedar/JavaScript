1fa1ec38833790c662bb1151ca4ac99da58deb19
Sachin kedar <kedarsachin1020@gmail.com>
2025-01-06 20:03:05 +0530

    Completed

--- Objects.js
+++ Objects.js
@@ -40,4 +40,75 @@ console.log(x);
 
 */
 
+/*
+Singleton Object or Object Constructor
 
+const tinder = new Object();
+
+
+tinder.id = "123";
+tinder.name = "Sam";
+tinder.isLoggedIn = false;
+// console.log(tinder);
+
+
+const regulaeUse = {
+    email : "abc@gmail.com",
+    "Full Name": {
+        userFullName: {
+            fName:"Amy",
+            lName:"Jackson"
+        }
+    },
+    fullname: {
+        userFullName: {
+            fName:"Sachin",
+            lName:"Kedar"
+        }
+    }
+}
+
+console.log(regulaeUse.fullname.userFullName.fName);
+
+console.log(regulaeUse["Full Name"]["userFullName"]["lName"]);
+
+console.log(regulaeUse["Full Name"].userFullName.fName)
+
+
+
+
+// Combining the Objects:
+
+const obj1 = {1:"a",2:"b",3:"c"}
+
+const obj2 = {4:"a",5:"b",6:"c"}
+
+const obj3 = {7:"a",8:"b",9:"c"}
+
+
+const obj4 = Object.assign({},obj1,obj2,obj3);
+// Here 1st parameter is a target in which we'll store all objects
+// 2nd parameter is a Source of objects.
+// If we don't add epmty object in above syntax it will store object in obj1
+
+// console.log(obj4)
+// console.log(obj4===obj1); // false (if added target as empty object)
+
+// Using Spread Operator:
+
+const obj5 = {...obj1, ...obj2,...obj3};
+console.log(obj5);
+
+*/
+
+/*
+De-structuring of an Object:
+*/
+
+const course = {
+    cname: "Js",
+    price: 999,
+    cInstucture: "Hitesh"
+}
+
+ 
